44f9d9b0c0b8549a021324729e02bfaa0495f199
Abidazhar <muhammad.abid.azhar24051@mhs.uingusdur.ac.id>
2025-12-12 16:30:29 +0700

    Inisialisasi

---
+++ Galeri.js
@@ -0,0 +1,38 @@
+// =========================== galeri.js ==============================================
+
+const cards = [...document.querySelectorAll(".card")];
+
+class Galeri {
+  constructor(element) {
+    this.element = element;
+    this.image = element.querySelector("img");
+    this.caption = element.querySelector("p");
+
+    // Simpan teks asli dan di swmbunyikan terlebih dahulu
+    this.originalText = this.caption.textContent;
+    this.caption.textContent = ""; // di sembunyikan awal
+
+    // Status toggle
+    this.isVisible = false;
+
+    // Event click dan hover
+    this.image.addEventListener("click", () => this.toggleNama());
+
+    this.element.addEventListener("mouseenter", () =>
+      this.element.classList.add("hover")
+    );
+
+    this.element.addEventListener("mouseleave", () =>
+      this.element.classList.remove("hover")
+    );
+  }
+
+  toggleNama() {
+    const { alt } = this.image; // Destructuring alt
+
+    this.caption.textContent = this.isVisible ? "" : `📌 Nama Produk: ${alt}`;
+    this.isVisible = !this.isVisible;
+  }
+}
+
+cards.forEach(card => new Galeri(card));
---
+++ script.js
@@ -0,0 +1,21 @@
+alert('Selamat Datang di Website Profil UMKM EcoBag Nusantara')
+
+const namaUMKM = 'EcoBag Nusantara'
+console.log('Nama UMKM:', namaUMKM);
+
+let produk = 3;
+console.log('Jumlah Produk saat ini:', produk);
+
+produk+= 2;
+console.log('Setelah penembahan:', produk)
+
+// Tambahan: jumlah produk perjenis
+produk+= 2;
+
+// tambahan jumlah produk per jenis
+let toteBag = 2;
+let slingBag = 1;
+let dompet = 2;
+
+let totalJenis = toteBag + slingBag + dompet;
+console.log('Jumlah produk berdasarkan jenis:', totalJenis);
